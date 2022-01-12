bf427bc4a08956c85dbd93768340934c8944bbe5
chetanbs1987pune <chetanbs1987pune@gmail.com>
2022-01-12 11:29:10 +0530

    unwanted lines removed

--- src/test/java/testNGTest/loksattaFeatures.java
+++ src/test/java/testNGTest/loksattaFeatures.java
@@ -3,7 +3,7 @@ package testNGTest;
 import java.io.IOException;
 import java.util.Properties;
 
-import javax.lang.model.element.Element;
+
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
